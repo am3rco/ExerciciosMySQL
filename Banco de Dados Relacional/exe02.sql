55ec6734c903b1dec1436739bc4b1992052389cf
am3rco <am3rco@gmail.com>
2023-03-09 09:53:55 -0300

    Pizzaria

--- Banco de Dados Relacional/exe02.sql
+++ Banco de Dados Relacional/exe02.sql
@@ -13,6 +13,12 @@ INSERT INTO tb_categorias(tipo, descrição)
 VALUES('Salgada', 'Deliciosas Pizzas Salgadas da Pizzaria Sorriso');
 INSERT INTO tb_categorias(tipo, descrição) 
 VALUES('Doce', 'Deliciosas Pizzas Doces da Pizzaria Sorriso');
+INSERT INTO tb_categorias(tipo, descrição) 
+VALUES('Vegetariana', 'Deliciosas Pizzas Salgadas da Pizzaria Sorriso');
+INSERT INTO tb_categorias(tipo, descrição) 
+VALUES('Vegana', 'Deliciosas Pizzas Doces da Pizzaria Sorriso');
+INSERT INTO tb_categorias(tipo, descrição) 
+VALUES('Calzone', 'Deliciosas Pizzas Doces da Pizzaria Sorriso');
 
 CREATE TABLE tb_pizzas(
 	id int auto_increment,
@@ -25,22 +31,22 @@ CREATE TABLE tb_pizzas(
 	PRIMARY KEY(id)
 );
 
-INSERT INTO tb_pizzas (sabor, ingredientes, tamanho, preço, categoria_id) 
-VALUES ('Mussarela', 'Molho de tomate, mussarela e orégano', 'Grande', 49.90, 1);
-INSERT INTO tb_pizzas (sabor, ingredientes, tamanho, preço, categoria_id) 
-VALUES ('Calabresa', 'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano', 'Média', 28.90, 1);
-INSERT INTO tb_pizzas (sabor, ingredientes, tamanho, preço, categoria_id) 
-VALUES ('Frango Catupiry', 'Molho de tomate, mussarela, frango desfiado, catupiry e orégano', 'Grande', 50, 1);
-INSERT INTO tb_pizzas (sabor, ingredientes, tamanho, preço, categoria_id) 
-VALUES ('Portuguesa', 'Molho de tomate, mussarela, presunto, ovos, cebola, pimentão, azeitona e orégano', 'Grande', 51.90, 1);
-INSERT INTO tb_pizzas (sabor, ingredientes, tamanho, preço, categoria_id) 
-VALUES ('Chocolate com Morango', 'Chocolate ao leite, morango e leite condensado', 'Grande', 55.00, 2);
-INSERT INTO tb_pizzas (sabor, ingredientes, tamanho, preço, categoria_id) 
-VALUES ('Banana com Canela', 'Banana, canela, açúcar e leite condensado', 'Média', 40.90, 2);
-INSERT INTO tb_pizzas (sabor, ingredientes, tamanho, preço, categoria_id) 
-VALUES ('Romeu e Julieta', 'Queijo branco, goiabada e leite condensado', 'Grande', 60.90, 2);
-INSERT INTO tb_pizzas (sabor, ingredientes, tamanho, preço, categoria_id) 
-VALUES ('Quatro Queijos', 'Mussarela, provolone, cheddar e catupiry', 'Grande', 60.90, 1);
+INSERT INTO tb_pizzas (sabor, ingredientes, tamanho, preço, categoria_id)
+VALUES ('Mussarela', 'Molho de tomate, mussarela, orégano', 'Grande', 49.90, 1);
+INSERT INTO tb_pizzas (sabor, ingredientes, tamanho, preço, categoria_id)
+VALUES ('Calabresa', 'Molho de tomate, mussarela, calabresa, cebola, orégano', 'Grande', 51.50, 1);
+INSERT INTO tb_pizzas (sabor, ingredientes, tamanho, preço, categoria_id)
+VALUES ('Frango com Catupiry', 'Molho de tomate, mussarela, frango, catupiry, orégano', 'Grande', 55.90, 1);
+INSERT INTO tb_pizzas (sabor, ingredientes, tamanho, preço, categoria_id)
+VALUES ('Chocolate', 'Chocolate ao leite, leite condensado', 'Broto', 32.50, 2);
+INSERT INTO tb_pizzas (sabor, ingredientes, tamanho, preço, categoria_id)
+VALUES ('Banana com Canela', 'Banana, açúcar, canela', 'Grande', 60.90, 2);
+INSERT INTO tb_pizzas (sabor, ingredientes, tamanho, preço, categoria_id)
+VALUES ('Vegetariana', 'Molho de tomate, mussarela, tomate, cebola, pimentão, orégano', 'Grande', 52.90, 3);
+INSERT INTO tb_pizzas (sabor, ingredientes, tamanho, preço, categoria_id)
+VALUES ('Vegana', 'Molho de tomate, tofu, cebola, pimentão, orégano', 'Grande', 60.90, 4);
+INSERT INTO tb_pizzas (sabor, ingredientes, tamanho, preço, categoria_id)
+VALUES ('Calzone de Frango', 'Molho de tomate, mussarela, frango, catupiry, orégano', 'Único', 30.90, 5);
 
 SELECT*FROM tb_pizzas WHERE preço > 45.00;
 SELECT*FROM tb_pizzas WHERE preço BETWEEN 50.00 and 100.00;
